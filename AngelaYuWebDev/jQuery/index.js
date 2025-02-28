1751b9490708d79981176bd9494982595f33c692
Ripudaman Bansal <ripudamanbansal20@gmail.com>
2025-02-28 19:26:15 +0530

    course conti.

---
+++ AngelaYuWebDev/jQuery/index.js
@@ -0,0 +1,11 @@
+$("h1").css("color", "red");
+//$("h1").text("textCHanged");
+
+$("a").attr("href", "https://yahoo.com");
+$("h1").click(function () {
+  $("h1").css("color", "violet");
+});
+
+$("button").click(function () {
+  $("button").css("background-color", "purple");
+});
